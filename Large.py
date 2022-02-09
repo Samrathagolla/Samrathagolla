db3a7974eddf5ea87a6c1caf62885d19893c5597
Samrathagolla <99330181+Samrathagolla@users.noreply.github.com>
2022-02-09 20:50:09 +0530

    Create Large.py

---
+++ Large.py
@@ -0,0 +1,11 @@
+​a=int(input('enter the value of a:')) 
+ ​b=int(input('enter the value of b:')) 
+ ​if a>=b: 
+ ​  print('a is biggest number') 
+ ​else:  
+ ​  print('a is smallest number')
+_______________________________
+________________________________      
+ Output  enter the value of a:10 
+   enter the value of b: 5   
+     a is the biggest number                                  
